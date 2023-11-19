0aae4fdde4c7b8d2022ceb4c70597a6ec148fe83
namanj2003 <namankjain2003@gmail.com>
2023-11-19 16:19:47 +0530

    Update AuthTokenRequired.js

--- Middleware/AuthTokenRequired.js
+++ Middleware/AuthTokenRequired.js
@@ -4,21 +4,19 @@ const User = mongoose.model("User");
 require("dotenv").config();
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization) {
-    return res.status(401).send({ error: "You must be logged in, key not given" });
-  }
-  const token = authorization.replace("Bearer ", "");
+  // const { authorization } = req.headers;
+  // if (!authorization) {
+  //   return res.status(401).send({ error: "You must be logged in, key not given" });
+  // }
+  // const token = authorization.replace("Bearer ", "");
   // console.log(token);
   jwt.verify(token, process.env.jwt_secret, (err, payload) => {
     if (err) {
-      return res
-        .status(401)
-        .send({ error: "You must be logged in, token invalid" });
+      return res.status(401).send({ error: "You must be logged in, token invalid" });
     }
     const { _id } = payload;
     User.findById(_id).then((userdata) => {
-        // console.log(userdata);
+        console.log(userdata);
       req.user = userdata;
       next();
     });
