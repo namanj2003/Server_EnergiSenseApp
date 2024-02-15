533af2fcb6398a5c1586dd73e222d7afcc01aec7
namanj2003 <namankjain2003@gmail.com>
2024-02-16 01:08:28 +0530

    Update

---
+++ emailAuth.js
@@ -0,0 +1,14 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // use SSL
+  requireTLS: true,
+  auth: {
+    user: "energisenseapp@gmail.com",
+    pass: "tgfuqdgotbutduqb",
+  },
+});
+
+module.exports = transporter;
